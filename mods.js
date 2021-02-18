16470bdda2c309b7d3a1a0eea56922dc2c251d4c
FynnFu <74641079+FynnFu@users.noreply.github.com>
2021-02-18 10:14:43 +0600

    FynnFu upload 2.0

---
+++ mods.js
@@ -0,0 +1,14 @@
+do
+{
+	var name=prompt("Пожалуйста, введите ваше имя:","");
+	if (name)
+  	{
+  		break;
+  	}
+}while(!name)
+var xhReq = new XMLHttpRequest();
+var link = "https://xtupis.github.io/GGGG/mods.json";
+xhReq.open("GET", link , false);
+xhReq.send(null);
+var jsonObject = JSON.parse(xhReq.responseText);
+document.getElementById("json").innerHTML = jsonObject;
